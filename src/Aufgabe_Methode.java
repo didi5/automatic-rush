ed318c8a3873e9c031293e4bc8b9f53959d574ce
Dilem <dilemkaya>
2019-08-17 09:56:58 +0200

    test

---
+++ src/Aufgabe_Methode.java
@@ -0,0 +1,25 @@
+
+public class Aufgabe_Methode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double zahl1 = 3.547;
+		double zahl2 = 6.354;
+		
+		double ergebnis = produkt(zahl1, zahl2);
+		
+		System.out.println("Ergebnis: " + ergebnis);
+	}
+	
+	public static double produkt (double zahl1, double zahl2) {
+		
+		double ergebnis = zahl1* zahl2;
+		
+		
+		
+		
+		return ergebnis;
+	}
+	
+}
